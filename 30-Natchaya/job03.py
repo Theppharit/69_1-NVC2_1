586cf9fc9d51d587e6229bd57c105581e4bb5c4b
nxghtz <pongphiphatyy@gmail.com>
2026-08-17 14:27:54 +0700

    +

---
+++ 30-Natchaya/job03.py
@@ -0,0 +1,12 @@
+price = float(input("ค่าคะแนนของนักเรียน1คน : "))
+
+if price >= 80 :
+    print("A")
+elif price >= 70 :
+    print("B")
+elif price >= 60 :
+    print("C")
+elif price >=50  :
+    print("D")
+else :
+    print("F")
