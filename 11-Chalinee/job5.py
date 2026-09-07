4c94c6929c0fed36e83f177b79c25301fab2274f
prapapan24 <Prapapan6818@gmail.com>
2026-09-07 15:31:01 +0700

    +

--- 11-Chalinee/job5.py
+++ 11-Chalinee/job5.py
@@ -1,4 +1,1 @@
-friends = ["วิว จิรัชญา", "แอฟ ชาลิณี ", "ติม ชาลิสา", "เก้า ฐิตาพร", "อิ๋ง สุชานันทร์"]
-
-for i, name in enumerate(friends, start=1):
-    print("ลำดับที่ " + str(i) + " " + name)
+11-Chalinee/13-chalini
