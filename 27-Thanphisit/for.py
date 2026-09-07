b8adc6922d3bfb32dd899e876f73f3aa7697c3cf
nongaun-acb <oza07630@gmail.com>
2026-09-07 14:29:16 +0700

    52

---
+++ 27-Thanphisit/for.py
@@ -0,0 +1,16 @@
+# for i in range(9):
+#     print("ลำดับที่ " + str(i + 1))
+
+# for i in range(3, 5):
+#     print(i)
+
+officer = ["A", "B", "C"]
+
+for name in officer:
+    print(name)
+print("จบการทำงาน")
+
+students = ["ฟอร์ม", "ฟูม", "ฟิล์ม", "ฟาร์ม", "โฟม", "เฟรม"]
+
+for s in students:
+    print("ลำดับที่ " + s)
