99083c8584f2423322abbcd99bec5d0e4e0159e9
prapapan24 <Prapapan6818@gmail.com>
2026-09-07 15:34:26 +0700

    Update job5.py

--- 11-Chalinee/job5.py
+++ 11-Chalinee/job5.py
@@ -1,1 +1,4 @@
-11-Chalinee/13-chalini
+friends = ["ติม ชาลิสา", "แอฟ ชาลิณี", "วิว จิรัชญา", "กัญญ์วรา กัญญา", "อิ๋ง สุชานันทร์"]
+
+for i, name in enumerate(friends, start=1):
+    print("ลำดับที่ " + str(i) + " " + name)
