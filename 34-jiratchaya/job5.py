87fa3704dbab2738d00259377605ab7f27950f3a
prapapan24 <Prapapan6818@gmail.com>
2026-09-07 15:03:50 +0700

    Create job5.py

---
+++ 34-jiratchaya/job5.py
@@ -0,0 +1,4 @@
+friends = ["อิ๋ง สุชานันท์", "กวย หัวข้าว", "กันยา กัญญ์วรา", "ติม ชาลิสา", "แอป ชาลิณี"]
+
+for i, name in enumerate(friends, start=1):
+    print("ลำดับที่ " + str(i) + " " + name)
