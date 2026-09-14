0c68cbb90cc53734d4d7827bd8f80cfb7a41801a
Th.To. <96429296+Theppharit@users.noreply.github.com>
2026-09-14 13:26:55 +0700

    +

---
+++ 00-theppharit/while-loop.py
@@ -0,0 +1,16 @@
+# Control Structures in Python
+
+# Loops in Python
+# การใช้ลูป for และ while ใน Python
+
+# count = 0
+
+# while count < 5:
+#     print("Count is:", count)
+#     count += 1
+
+count = 0
+
+while count <= 5:
+    print("count = ", count)
+    count += 1
