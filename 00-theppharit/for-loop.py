84fa8e7c2162dff9de7b40b3762728c084ae0101
Th.To. <96429296+Theppharit@users.noreply.github.com>
2026-09-14 12:06:20 +0700

    +

--- 00-theppharit/for-loop.py
+++ 00-theppharit/for-loop.py
@@ -4,13 +4,8 @@
 # for i in range(3, 5):
 #     print(i)
 
-# officer = ["A", "B", "C"]
+officer = ["A", "B", "C"]
 
-# for name in officer:
-#     print(name)
-# print("จบการทำงาน")
-
-students = ["ฟอร์ม", "ฟูม", "ฟิล์ม", "ฟาร์ม", "โฟม" ,"เฟรม"]
-
-for s in students:
-    print("สวัสดีคุณ " + s)
+for name in officer:
+    print(name)
+print("จบการทำงาน")
